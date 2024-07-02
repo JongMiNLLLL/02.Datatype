0597fc78fa2c9da3e4979db9923a4f8f1d2fa8c7
JongMiNLLLL <jml9716@naver.com>
2024-07-02 17:25:45 +0900

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -18,7 +18,8 @@ dic_2 = {name_2 : [age_2,email_2,phone_2]}
 print(dic_2)
 
 #Quiz 2
-exchange = {"달러": 1320, "엔":950, "위안":182}
-money = [13,200,13]
+exchange = {"달러": 1320, "엔": 950, "위안": 182}
+money = [13, 200, 13]
 
-print(exchange['달러'] * money[0] + exchange['엔'] * money[1] + exchange['위안'] * money[2])
+total = exchange['달러'] * money[0] + exchange['엔'] * money[1] + exchange['위안'] * money[2]
+print('가지고 계신 돈은' , total,   '원 입니다.')
